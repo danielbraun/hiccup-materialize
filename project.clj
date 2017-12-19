7b436c323e2fdb255bd5b3cac1b7a5083a3051ed
Daniel Braun <danielbraun@me.com>
2017-12-19 15:13:40 +0200

    Added URL and description to project def

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hiccup-materialize "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Hiccup components for the Materialize front-end framework."
+  :url "https://github.com/danielbraun/hiccup-materialize"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
